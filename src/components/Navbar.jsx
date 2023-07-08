5aec42eff26cb5a94e3c2539310f983728eecd9b
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2023-07-08 16:40:21 +0300

    up

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { styled } from 'styled-components'
 
+const Container = styled.div`
+height: 60px;
+`;
+const Wrapper = styled.div`
+padding: 10px 20px;
+`;
 
+const Left = styled.div``;
+const Center = styled.div``;
+const Right = styled.div``;
 
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <Container>
+            <Wrapper>
+                <Left>left</Left>
+                <Center>Center</Center>
+                <Right>Right</Right>
+            </Wrapper>
+        </Container>
     )
 }
 
