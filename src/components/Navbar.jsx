020baf23f76506311fad72f365ad3927887930d3
alinukerotester <109532807+alinukerotester@users.noreply.github.com>
2023-07-12 11:29:25 +0300

    added also material ui

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,17 +6,37 @@ height: 60px;
 `;
 const Wrapper = styled.div`
 padding: 10px 20px;
+display: flex;
+justify-content: space-between;
 `;
 
-const Left = styled.div``;
-const Center = styled.div``;
-const Right = styled.div``;
+const Left = styled.div`
+flex: 1;
+`;
+
+const Language = styled.span`
+font-size: 14px;
+cursor: pointer;
+`;
+
+const Center = styled.div`
+flex: 1;
+`;
+const Right = styled.div`
+flex: 1;
+`;
 
 const Navbar = () => {
     return (
         <Container>
             <Wrapper>
-                <Left>left</Left>
+                <Left>
+                    <Language>EN</Language>
+                    <SearchContainer>
+                        input
+                        icon
+                    </SearchContainer>
+                </Left>
                 <Center>Center</Center>
                 <Right>Right</Right>
             </Wrapper>
